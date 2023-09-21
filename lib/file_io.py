b66c32d5468f884adb658174bcea27e075e9e77b
William Nyongesa <marakawilliam@gmail.com>
2023-09-21 21:21:51 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as write_file:
+        write_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a', encoding='utf-8') as append_file:
+        append_file.write(append_content) 
+
 
 def read_file(file_name):
-    pass
+    
+        with open(f'{file_name}.txt', 'r', encoding='utf-8') as read_file:
+            content = read_file.read()
+        return content
+    
